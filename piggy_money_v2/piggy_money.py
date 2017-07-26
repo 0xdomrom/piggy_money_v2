f64130e66702a778c35217fcf9e193902f84f859
DominicRomanowski <romanowski.dominic@gmail.com>
2017-07-26 10:52:30 +1000

    starting vue app

--- piggy_money_v2/piggy_money.py
+++ piggy_money_v2/piggy_money.py
@@ -6,14 +6,14 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(__name__)
 
-    #app.config.update(dict())
+    app.config.update(dict())
 
-    app.config.from_envvar('PIGGY_MONEY_SETTINGS', silent=False)
+    #app.config.from_envvar('PIGGY_MONEY_SETTINGS', silent=False)
     register_blueprints(app)
 
     return app
 
-
+    
 def register_blueprints(app):
     for name in find_modules('piggy_money_v2.blueprints'):
         module = import_string(name)
